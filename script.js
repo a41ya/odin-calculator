fee1435a82b427ef8aac77fb447fe0eda064213e
a41ya <180646616+a41ya@users.noreply.github.com>
2026-09-19 11:07:34 +0300

    add operate function

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ const multiply = function(a, b) {
 const divide = function(a, b) {
 	return a / b;
 };
+
+function operate (first, second, operation){
+  if (operation === "+"){
+    return add(first, second);
+  } else if (operation === "-"){
+    return subtract(first, second);
+  } else if (operation === "*"){
+    return multiply(first, second);
+  } else if (operation === "/"){
+    return divide(first, second);
+  }
+}
+
+console.log(operate(5, 7, "+"))
